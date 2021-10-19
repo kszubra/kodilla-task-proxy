47c586de8c7ab0f359c7eb591b7c6151c3fd33d4
Kacper Szubra <kacper.szubra@nvt.pl>
2021-10-19 18:57:42 +0200

    initial commit

---
+++ src/test/java/com/kodilla/task/weatherproxy/WeatherProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kodilla.task.weatherproxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
